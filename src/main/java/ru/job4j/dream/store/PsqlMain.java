c617b91b6d9bd4ee7b1088e550bbb8efc8f9c8ea
Istern22 <alistern22@gmail.com>
2021-02-08 18:01:30 +1000

    PsqlStore. [#388774]

---
+++ src/main/java/ru/job4j/dream/store/PsqlMain.java
@@ -0,0 +1,26 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
+
+public class PsqlMain {
+    public static void main(String[] args) {
+        Store store = PsqlStore.instOf();
+        store.save(new Post(0, "Junior Job"));
+        store.save(new Post(0, "Middle Job"));
+        store.save(new Post(0, "Senior Job"));
+        System.out.println("Все вакансии: ");
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+        System.out.println("Вакансия с индексом 2: " + store.findPostById(2).toString());
+        store.save(new Candidate(0, "Junior"));
+        store.save(new Candidate(0, "Middle"));
+        store.save(new Candidate(0, "Senior"));
+        System.out.println("Все кандидаты: ");
+        for (Candidate candidate : store.findAllCandidates()) {
+            System.out.println(candidate.getId() + " " + candidate.getName());
+        }
+        System.out.println("Вакансия с индексом 1: " + store.findCandidateById(1).toString());
+    }
+}
